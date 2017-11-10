9770934bd6c5405c43df9bccefe1d09f41454e4d
JackSix <gyacalis@uci.edu>
2017-11-10 15:33:28 -0800

    Make run_loop_on_data output 10 random examples instead of just one example

--- f90_autogen/run_loop_on_data.py
+++ f90_autogen/run_loop_on_data.py
@@ -6,6 +6,7 @@ Created on Wed Nov 8 15:59:30 2017
 @author: Yacalis
 """
 
+import numpy as np
 from ConvoDataLoader import ConvoDataLoader
 from Config import Config
 from f90_autogen.python_convo_loop import chckpt_ver_predict_spdt_spdq
@@ -43,18 +44,22 @@ def main():
     x_data, y_data = dataloader.get_data()
     print('x_data shape: ', x_data.shape)
     print('y_data shape: ', y_data.shape)
-
-    print('Running Convo Loop on SP-CAM Data with Chkpt Filters and Biases')
-    state = x_data[0][:, 0, :]
-    new_state = chckpt_ver_predict_spdt_spdq(state.tolist(), filters, biases)
     print('filters size: ', len(filters), len(filters[0]), len(filters[0][0]), len(filters[0][0][0]), len(filters[0][0][0][0]))
     print('biases size: ', len(biases), len(biases[0]))
+
+    print('Running Convo Loop on SP-CAM Data with Chkpt Filters and Biases')
+    new_state_arr = []
+    y_data_arr = []
+    for sample in range(10):
+        rand_samp = np.random.randint(0, x_data.shape[0])
+        state = x_data[rand_samp][:, 0, :]
+        new_state = chckpt_ver_predict_spdt_spdq(state.tolist(), filters, biases)
+        new_state_arr.append(new_state)
+        y_data_arr.append(y_data[rand_samp+1][:, 0, :].tolist())
+    print('pred: ', new_state_arr)
+    print('actual: ', y_data_arr)
     print('input state size: ', len(state), len(state[0]))
     print('output state size: ', len(new_state), len(new_state[0]))
-    print('new_state, then actual SPDT/SPDQ profile')
-    print(new_state)
-    print(y_data[1][:, 0, :].tolist())
-
     print('Done.')
 
 
